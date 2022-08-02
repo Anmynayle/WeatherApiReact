4a085676b56f998d29fbb782c5bff6e62c863e5a
Anmy Moreno <nayle2701@gmail.com>
2022-08-01 20:11:21 -0400

    star

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Card__Weather from './components/Card__Weather'
+
+
+
+function App() {
+
+ const [coords, setCoords] = useState()
+ const [urlImg, setUrlImg] = useState()
+
+ useEffect(()=>{
+
+  const success = pos =>{
+    //console.log(pos.coords.latitude)
+    //console.log(pos.coords.longitude)
+    const latlon={
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    }
+    setCoords(latlon)
+  }
+  navigator.geolocation.getCurrentPosition(success)
+
+ },[])
+ console.log(urlImg)
+
+  return (
+    <div  className="App"> 
+      <Card__Weather coords={coords}/>
+    </div>
+    
+  )
+}
+
+export default App
